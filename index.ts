f3340d3de3e2cbb0f79ad8f8042ea4ea1b7c7653
Jon Jensen <jenseng@gmail.com>
2025-05-23 16:57:08 -0600

    debug process list

--- index.ts
+++ index.ts
@@ -4,18 +4,17 @@ import find from "find-process";
 import { globby } from "globby";
 
 try {
-  const results = await find("name", "Runner.worker");
+  const results = await find("name", "Runner.Worker");
   if (results.length !== 1) {
-    throw new Error(
-      `Expected exactly one Runner.worker process, but found ${results.length}`,
-    );
+    console.warn(results);
+    throw new Error(`Expected exactly one Runner.worker process, but found ${results.length}`);
   }
 
   const workerCmd = results[0]!.cmd;
   const index = workerCmd.indexOf("/bin/Runner.worker");
   if (index === -1) {
     throw new Error(
-      `Unable to extract path from Runner.worker command string, this might be a bug (${workerCmd})`,
+      `Unable to extract path from Runner.worker command string, this might be a bug (${workerCmd})`
     );
   }
 
@@ -25,22 +24,17 @@ try {
     absolute: true,
   });
   if (workerLogFiles.length !== 1) {
+    console.warn(workerLogFiles);
     throw new Error(
-      `Expected exactly one Runner.worker log file, but found ${
-        workerLogFiles.length
-      }`,
+      `Expected exactly one Runner.worker log file, but found ${workerLogFiles.length}`
     );
   }
 
   const workerLogFile = await fs.readFile(workerLogFiles[0]!, "utf8");
   const lines = workerLogFile.split("\n");
-  const jobIdLine = lines.find((line) =>
-    line.includes("INFO JobRunner] Job ID "),
-  );
+  const jobIdLine = lines.find((line) => line.includes("INFO JobRunner] Job ID "));
   if (!jobIdLine) {
-    throw new Error(
-      "Unable to find job ID in Runner.worker log file, this might be a bug",
-    );
+    throw new Error("Unable to find job ID in Runner.worker log file, this might be a bug");
   }
   const uuid = jobIdLine.split("INFO JobRunner] Job ID ")[1]!;
 
