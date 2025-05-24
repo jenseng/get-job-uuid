1f4412c80415c8c3d1a44c5044dcd4f740437655
Jon Jensen <jenseng@gmail.com>
2025-05-23 19:04:22 -0600

    fix all the things (#1)

--- index.ts
+++ index.ts
@@ -3,48 +3,48 @@ import * as path from "path";
 import find from "find-process";
 import { globby } from "globby";
 
+const processName = "Runner.Worker";
 try {
-  const results = await find("name", "Runner.worker");
+  const results = await find("name", processName);
   if (results.length !== 1) {
-    throw new Error(
-      `Expected exactly one Runner.worker process, but found ${results.length}`,
-    );
+    console.warn(results);
+    throw new Error(`Expected exactly one Runner.worker process, but found ${results.length}`);
   }
+  console.log(results);
 
   const workerCmd = results[0]!.cmd;
-  const index = workerCmd.indexOf("/bin/Runner.worker");
+  const index = workerCmd.indexOf(path.join("bin", processName));
   if (index === -1) {
     throw new Error(
-      `Unable to extract path from Runner.worker command string, this might be a bug (${workerCmd})`,
+      `Unable to extract path from ${processName} command string, this might be a bug (${workerCmd})`
     );
   }
 
-  const runnerDir = workerCmd.slice(0, index);
-  const workerLogFiles = await globby("Worker_*.log", {
-    cwd: path.join(runnerDir, "_diag"),
-    absolute: true,
-  });
-  if (workerLogFiles.length !== 1) {
-    throw new Error(
-      `Expected exactly one Runner.worker log file, but found ${
-        workerLogFiles.length
-      }`,
-    );
+  const runnerDir = workerCmd.slice(0, index).replace(/^"/, ""); // on windows the bin is quoted
+  const workerLogFiles = (
+    await globby("Worker_*.log", {
+      cwd: path.join(runnerDir, "_diag"),
+      absolute: true,
+    })
+  )
+    .sort()
+    .reverse();
+
+  if (workerLogFiles.length === 0) {
+    throw new Error(`Unable to find ${processName} log file(s), this might be a bug`);
   }
 
-  const workerLogFile = await fs.readFile(workerLogFiles[0]!, "utf8");
-  const lines = workerLogFile.split("\n");
-  const jobIdLine = lines.find((line) =>
-    line.includes("INFO JobRunner] Job ID "),
-  );
-  if (!jobIdLine) {
-    throw new Error(
-      "Unable to find job ID in Runner.worker log file, this might be a bug",
-    );
+  for (const file of workerLogFiles) {
+    const content = await fs.readFile(file, "utf8");
+    const lines = content.split("\n");
+    const jobIdLine = lines.find((line) => line.includes("INFO JobRunner] Job ID "));
+    if (jobIdLine) {
+      const uuid = jobIdLine.split("INFO JobRunner] Job ID ")[1]!;
+      await fs.appendFile(process.env.GITHUB_OUTPUT!, `uuid=${uuid}\n`);
+      process.exit(0);
+    }
+    throw new Error(`Unable to find job ID in ${processName} log file(s), this might be a bug`);
   }
-  const uuid = jobIdLine.split("INFO JobRunner] Job ID ")[1]!;
-
-  await fs.appendFile(process.env.GITHUB_OUTPUT!, `uuid=${uuid}\n`);
 } catch (e) {
   console.error(`::error::${e}`);
   process.exit(1);
